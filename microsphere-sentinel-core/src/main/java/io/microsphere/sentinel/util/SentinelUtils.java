5e2deaec032076051787447d960387bb6ee5fdc7
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-12-04 17:11:53 +0800

    Update SentinelUtils.java

--- microsphere-sentinel-core/src/main/java/io/microsphere/sentinel/util/SentinelUtils.java
+++ microsphere-sentinel-core/src/main/java/io/microsphere/sentinel/util/SentinelUtils.java
@@ -1,9 +1,21 @@
 package io.microsphere.sentinel.util;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.Tracer;
+import com.alibaba.csp.sentinel.context.Context;
+import com.alibaba.csp.sentinel.context.ContextUtil;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
 import java.util.StringJoiner;
+import java.util.concurrent.Callable;
 
+import static com.alibaba.csp.sentinel.Constants.CONTEXT_DEFAULT_NAME;
 import static io.microsphere.util.ClassUtils.getSimpleName;
+
 
 /**
  * Alibaba Sentinel Utilities Class
@@ -12,6 +24,8 @@ import static io.microsphere.util.ClassUtils.getSimpleName;
  * @since 1.0.0
  */
 public abstract class SentinelUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(SentinelUtils.class);
 
     private SentinelUtils() {
     }
@@ -29,5 +43,50 @@ public abstract class SentinelUtils {
             resourceNameBuilder.add(getSimpleName(parameterType));
         }
         return resourceNameBuilder.toString();
+    }
+
+    /**
+     * Executes a callback in sentinel
+     *
+     * @param resourceName the resource of Sentinel's resource
+     * @param callback     callback
+     * @param <T>          the return type
+     * @return nullable
+     */
+    public static <T> T doInSentinel(String resourceName, Callable<T> callback) {
+        return doInSentinel(CONTEXT_DEFAULT_NAME, "", resourceName, callback);
+    }
+
+    /**
+     * Executes a callback in sentinel
+     *
+     * @param contextName  the name of {@link Context}
+     * @param origin       the origin of {@link Context}
+     * @param resourceName the resource of Sentinel's resource
+     * @param callback     callback
+     * @param <T>          the return type
+     * @return nullable
+     */
+    public static <T> T doInSentinel(String contextName, String origin, String resourceName, Callable<T> callback) {
+        T result = null;
+        ContextUtil.enter(contextName, origin);
+        Entry entry = null;
+        try {
+            entry = SphU.entry(resourceName);
+            result = callback.call();
+        } catch (Throwable e) {
+            if (!BlockException.isBlockException(e)) {
+                Tracer.trace(e);
+            }
+        } finally {
+            if (entry != null) {
+                entry.exit();
+            }
+            ContextUtil.exit();
+            if (logger.isDebugEnabled()) {
+                logger.debug("A callback : {} of Sentinel context[name :'{}' , origin : '{}'] resource[name :'{}'] was executed", callback, contextName, origin, resourceName);
+            }
+        }
+        return result;
     }
 }
