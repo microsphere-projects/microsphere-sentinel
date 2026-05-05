afaef6b3f48dd407809c71bc6e82cb156eda7402
Mercy Ma <mercyblitz@gmail.com>
2026-05-05 16:13:54 +0800

    Add Hibernate Sentinel plugin module
    Introduce a new microsphere-sentinel-hibernate plugin: add module pom and implementation to integrate Alibaba Sentinel with Hibernate. Files added include SentinelHibernateInterceptor, DelegatingInterceptor, SentinelHibernateIntegrator, service loader (META-INF/services), unit tests (SentinelHibernateInterceptorTest), test entity (User) and persistence.xml. Also register the new module in microsphere-sentinel-plugins/pom.xml. Additionally, update microsphere-sentinel-spring/pom.xml to use microsphere-sentinel-commons instead of microsphere-sentinel-core. The change enables Sentinel resource handling for common DB SQL within Hibernate operations and includes tests using H2 for verification.

---
+++ microsphere-sentinel-plugins/microsphere-sentinel-hibernate/src/main/java/io/microsphere/sentinel/hibernate/DelegatingInterceptor.java
@@ -0,0 +1,232 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.sentinel.hibernate;
+
+import io.microsphere.annotation.Nullable;
+import io.microsphere.lang.DelegatingWrapper;
+import org.hibernate.CallbackException;
+import org.hibernate.Interceptor;
+import org.hibernate.Transaction;
+import org.hibernate.metamodel.RepresentationMode;
+import org.hibernate.metamodel.spi.EntityRepresentationStrategy;
+import org.hibernate.type.Type;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+import static org.hibernate.internal.EmptyInterceptor.INSTANCE;
+
+/**
+ * Delegating {@link Interceptor}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Interceptor
+ * @since 1.0.0
+ */
+public class DelegatingInterceptor implements Interceptor, DelegatingWrapper {
+
+    protected final Interceptor delegate;
+
+    public DelegatingInterceptor() {
+        this(null);
+    }
+
+    public DelegatingInterceptor(@Nullable Interceptor delegate) {
+        this.delegate = delegate == null ? INSTANCE : delegate;
+    }
+
+    @Override
+    public boolean onLoad(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onLoad(entity, id, state, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onLoad(entity, id, state, propertyNames, types);
+    }
+
+    @Override
+    public boolean onPersist(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onPersist(entity, id, state, propertyNames, types);
+    }
+
+    @Override
+    public void onRemove(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        delegate.onRemove(entity, id, state, propertyNames, types);
+    }
+
+    @Override
+    public boolean onFlushDirty(Object entity, Object id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onSave(entity, id, state, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.6")
+    @Override
+    public boolean onSave(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        return delegate.onSave(entity, id, state, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        delegate.onDelete(entity, id, state, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.6")
+    @Override
+    public void onDelete(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
+        delegate.onDelete(entity, id, state, propertyNames, types);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public void onCollectionRecreate(Object collection, Serializable key) throws CallbackException {
+        delegate.onCollectionRecreate(collection, key);
+    }
+
+    @Override
+    public void onCollectionRecreate(Object collection, Object key) throws CallbackException {
+        delegate.onCollectionRecreate(collection, key);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public void onCollectionRemove(Object collection, Serializable key) throws CallbackException {
+        delegate.onCollectionRemove(collection, key);
+    }
+
+    @Override
+    public void onCollectionRemove(Object collection, Object key) throws CallbackException {
+        delegate.onCollectionRemove(collection, key);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public void onCollectionUpdate(Object collection, Serializable key) throws CallbackException {
+        delegate.onCollectionUpdate(collection, key);
+    }
+
+    @Override
+    public void onCollectionUpdate(Object collection, Object key) throws CallbackException {
+        delegate.onCollectionUpdate(collection, key);
+    }
+
+    @Override
+    public void preFlush(Iterator<Object> entities) throws CallbackException {
+        delegate.preFlush(entities);
+    }
+
+    @Override
+    public void postFlush(Iterator<Object> entities) throws CallbackException {
+        delegate.postFlush(entities);
+    }
+
+    @Override
+    public Boolean isTransient(Object entity) {
+        return delegate.isTransient(entity);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public int[] findDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+        return delegate.findDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    @Override
+    public int[] findDirty(Object entity, Object id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+        return delegate.findDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    @Override
+    public Object instantiate(String entityName, EntityRepresentationStrategy representationStrategy, Object id) throws CallbackException {
+        return delegate.instantiate(entityName, representationStrategy, id);
+    }
+
+    @Override
+    public Object instantiate(String entityName, RepresentationMode representationMode, Object id) throws CallbackException {
+        return delegate.instantiate(entityName, representationMode, id);
+    }
+
+    @Override
+    public String getEntityName(Object object) throws CallbackException {
+        return delegate.getEntityName(object);
+    }
+
+    @Deprecated(since = "6.0")
+    @Override
+    public Object getEntity(String entityName, Serializable id) throws CallbackException {
+        return delegate.getEntity(entityName, id);
+    }
+
+    @Override
+    public Object getEntity(String entityName, Object id) throws CallbackException {
+        return delegate.getEntity(entityName, id);
+    }
+
+    @Override
+    public void afterTransactionBegin(Transaction tx) {
+        delegate.afterTransactionBegin(tx);
+    }
+
+    @Override
+    public void beforeTransactionCompletion(Transaction tx) {
+        delegate.beforeTransactionCompletion(tx);
+    }
+
+    @Override
+    public void afterTransactionCompletion(Transaction tx) {
+        delegate.afterTransactionCompletion(tx);
+    }
+
+    @Override
+    public void onInsert(Object entity, Object id, Object[] state, String[] propertyNames, Type[] propertyTypes) {
+        delegate.onInsert(entity, id, state, propertyNames, propertyTypes);
+    }
+
+    @Override
+    public void onUpdate(Object entity, Object id, Object[] state, String[] propertyNames, Type[] propertyTypes) {
+        delegate.onUpdate(entity, id, state, propertyNames, propertyTypes);
+    }
+
+    @Override
+    public void onUpsert(Object entity, Object id, Object[] state, String[] propertyNames, Type[] propertyTypes) {
+        delegate.onUpsert(entity, id, state, propertyNames, propertyTypes);
+    }
+
+    @Override
+    public void onDelete(Object entity, Object id, String[] propertyNames, Type[] propertyTypes) {
+        delegate.onDelete(entity, id, propertyNames, propertyTypes);
+    }
+
+    @Override
+    public Interceptor getDelegate() {
+        return delegate;
+    }
+}
---
+++ microsphere-sentinel-plugins/microsphere-sentinel-hibernate/src/test/java/io/microsphere/sentinel/SentinelHibernateInterceptorTest.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.sentinel;
+
+import io.microsphere.sentinel.hibernate.SentinelHibernateInterceptor;
+import io.microsphere.sentinel.hibernate.test.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.StatelessSession;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * {@link SentinelHibernateInterceptor} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see SentinelHibernateInterceptor
+ * @since 1.0.0
+ */
+class SentinelHibernateInterceptorTest {
+
+    private SessionFactory sessionFactory;
+
+    private Session session;
+
+    private StatelessSession statelessSession;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(User.class)
+                .setProperty("jakarta.persistence.jdbc.driver", "org.h2.Driver")
+                .setProperty("jakarta.persistence.jdbc.url", "jdbc:h2:mem:test_db;DB_CLOSE_DELAY=-1")
+                .setProperty("jakarta.persistence.jdbc.user", "sa")
+                .setProperty("jakarta.persistence.jdbc.password", "")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
+                .setProperty("hibernate.format_sql", "true")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.hbm2ddl.auto", "update")
+                // .setInterceptor(new SentinelHibernateInterceptor())
+                ;
+
+        this.sessionFactory = configuration.buildSessionFactory();
+        this.session = sessionFactory.openSession();
+        this.statelessSession = sessionFactory.openStatelessSession();
+        this.user = new User();
+        this.user.setId(1);
+        this.user.setName("Mercy");
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.session.close();
+        this.statelessSession.close();
+        this.sessionFactory.close();
+    }
+
+    @Test
+    @DisplayName("Test Session: save & update & flush & get & delete")
+    void testSession1() {
+        int userId = this.user.getId();
+        assertEquals(userId, this.session.save(this.user));
+
+        this.user.setName("Ma");
+        this.session.update(this.user);
+
+        // flush
+        Transaction transaction = this.session.beginTransaction();
+        this.session.flush();
+        transaction.commit();
+
+        this.user = this.session.get(User.class, 1);
+        assertNotNull(this.user);
+
+        this.session.delete(this.user);
+    }
+
+    @Test
+    @DisplayName("Test : persist & saveOrUpdate & load & remove")
+    void testSession2() {
+        this.session.persist(this.user);
+
+        this.user.setId(2);
+        this.user.setName("Ma");
+        this.session.saveOrUpdate(this.user);
+
+        this.user = this.session.load(User.class, 1);
+        assertNotNull(this.user);
+
+        this.session.remove(this.user);
+    }
+
+    @Test
+    @DisplayName("Test : persist & find & merge & remove")
+    void testSession3() {
+        this.session.persist(this.user);
+
+        this.user = this.session.find(User.class, 1);
+        assertNotNull(this.user);
+
+        this.user.setId(2);
+        this.user.setName("Ma");
+        this.session.merge(this.user);
+
+        this.session.remove(this.user);
+    }
+
+    @Test
+    @DisplayName("Test Stateless Session: insert & update & get & delete")
+    void testStatelessSession1() {
+        int userId = this.user.getId();
+
+        // insert
+        assertEquals(userId, this.statelessSession.insert(this.user));
+
+        this.user.setName("Ma");
+        // update
+        this.statelessSession.update(this.user);
+
+        // upsert
+        this.statelessSession.upsert(this.user);
+
+        // get
+        assertEquals(this.user, this.statelessSession.get(User.class, userId));
+
+        // delete
+        this.statelessSession.delete(this.user);
+    }
+
+
+}
